a95732947407101fc2166c4297a2621f65975c85
hiba jebbar <hibajebbar6873@gmail.com>
2023-09-14 19:04:00 -0700

    457

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -1,28 +1,20 @@
-#include "holberton.h"
-
+#include "main.h"
 /**
- * main - check the code for Holberton School students.
- *
- * Return: Always 0.
- */
-int main(void)
+* print_number - Prints a number
+* @n: The number to print
+*
+*/
+void print_number(int n)
 {
-	print_number(9);
-	_putchar('\n');
-	print_number(-2);
-	_putchar('\n');
-	print_number(1024);
-	_putchar('\n');
-	print_number(0);
-	_putchar('\n');
-	print_number(-98);
-	_putchar('\n');
-	print_number(-2147483648);
-	_putchar('\n');
-	print_number(2147483647);
-	_putchar('\n');
-	print_number(-0);
-	_putchar('\n');
-	return (0);
+unsigned int num = n;
+if (n < 0)
+{
+_putchar('-');
+num = -num;
 }
-
+if (num > 9)
+{
+print_number(num / 10);
+}
+_putchar(num % 10 + '0');
+}
